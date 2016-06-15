bbd1246e0bde2319dfb40ebf32ab8a86ff212f4d
tibujr <maycol_to@hotmail.com>
2016-06-15 16:42:06 -0500

    des

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -7,22 +7,13 @@ module.exports = [
         "clobbers": [
             "PushbotsPlugin"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-mauron85-background-geolocation/www/backgroundGeolocation.js",
-        "id": "cordova-plugin-mauron85-background-geolocation.backgroundGeolocation",
-        "pluginId": "cordova-plugin-mauron85-background-geolocation",
-        "clobbers": [
-            "backgroundGeolocation"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.1",
-    "pushbots-cordova-plugin": "1.3.6",
-    "cordova-plugin-mauron85-background-geolocation": "2.0.0-rc.3"
+    "pushbots-cordova-plugin": "1.3.6"
 }
 // BOTTOM OF METADATA
 });
