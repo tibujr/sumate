88f5c9bbc560734a591cf7fbc072cafc9a90a073
tibujr <maycol_to@hotmail.com>
2016-06-23 15:28:00 -0500

    haber

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -128,7 +128,7 @@ var app = {
                 activityType: 'AutomotiveNavigation',
                 debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
                 stopOnTerminate: false, // <-- enable this to clear background location settings when the app terminates
-                locationProvider: backgroundGeolocation.provider[ENV.settings.locationProvider],
+                locationProvider: backgroundGeolocation.provider.ANDROID_ACTIVITY_PROVIDER,//backgroundGeolocation.provider[ENV.settings.locationProvider],
                 fastestInterval: 5000,
                 activitiesInterval: 10000
             });
